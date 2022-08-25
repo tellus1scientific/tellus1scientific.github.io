23b2fd08b9a6c6186c686439d1167134c7ab4b5d
tellus1scientific <111208723+tellus1scientific@users.noreply.github.com>
2022-08-24 20:00:18 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
+class NavbarHeader extends HTMLElement{
+    connectedCallback()
+    {
+        this.innerHTML = `
+            <nav>
+                <ul>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="ourteam.html">Our Team</a></li>
+                    <li><a href="#">(Contact)</a></li>
+                    <li><a href="#">(Works)</a></li>
+                    <li><a href="#">(Clients)</a></li>			
+                </ul>
+            </nav>
+            <br>
+        `
+    }
+}
+customElements.define('navbar-header', NavbarHeader)
 
+class TeamNavbarHeader extends HTMLElement{
+    connectedCallback()
+    {
+        this.innerHTML = `
+            <nav>
+                <ul>
+                    <li><a href="../index.html">Home</a></li>
+                    <li><a href="../ourteam.html">Our Team</a></li>
+                    <li><a href="#">(Contact)</a></li>
+                    <li><a href="#">(Works)</a></li>
+                    <li><a href="#">(Clients)</a></li>			
+                </ul>
+            </nav>
+            <br>
+        `
+    }
+}
+customElements.define('team-navbar-header', TeamNavbarHeader)
